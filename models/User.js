fd0abcb110146b4bfcf983687b9731e05c5138b0
Arjun Singh <arjunkhakh@gmail.com>
2022-05-04 20:39:02 +0100

    creating the user model

--- models/User.js
+++ models/User.js
@@ -7,6 +7,29 @@ const userSchema = new Schema(
             required: true,
             unique: true,
             trim: true,
-        }
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: /.+\@.+\..+/,
+        },
+        thoughts: [
+            {
+              type: Schema.Types.ObjectId,
+              ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+              type: Schema.Types.ObjectId,
+              ref: 'User',
+            },
+        ],
     },
 )
+
+
+const User = model('user', userSchema);
+
+module.exports = User;
